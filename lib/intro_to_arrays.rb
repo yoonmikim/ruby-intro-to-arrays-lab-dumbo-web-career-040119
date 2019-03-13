c0a5345fce65d8423b3ffc022ab5ea617a0a5cdd
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-13 06:19:05 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,35 @@
+
+
+def instantiate_new_array
+    @my_new_array = []
+    @my_new_array
+end
+  
+def array_with_two_elements
+ @my_two_array = [1, 2]
+end 
+
+    
+def first_element(arg)
+  @taylor_swift[0]
+end
+
+def third_element(arg)
+  @taylor_swift[2]
+  end
+def last_element(arg)
+  @taylor_swift[-1]
+end
+
+def first_element_with_array_methods(arg)
+  @south_east_asia[0]
+     
+end
+
+def last_element_with_array_methods(arg)
+      @south_east_asia[-1]
+end
+
+def length_of_array(arg)
+ @programming_languages.length
+end
