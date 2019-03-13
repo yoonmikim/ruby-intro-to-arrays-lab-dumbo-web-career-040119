c358f7da983cf9577ab2350fa1fcd04b9dc58032
clever-puppet-5071 <yoonmi.kim@rutgers.edu>
2019-03-13 06:13:28 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -8,9 +8,6 @@ end
 def array_with_two_elements
  @my_two_array = [1, 2]
 end 
-      
-
-      @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
 
     
 def first_element
@@ -24,21 +21,15 @@ def last_element
   @taylor_swift[-1]
 end
 
-      @south_east_asia = ["Thailand", "Cambodia", "Singapore", "Myanmar"]
-      
 def first_element_with_array_methods
   @south_east_asia[0]
      
-    end
+end
 
-    def last_element_with_array_methods
+def last_element_with_array_methods
       @south_east_asia[-1]
-  end
+end
 
-  
-      @programming_languages = ["Ruby", "Javascript", "Python", "C++", "Java", "Lisp", "PHP", "Clojure"]
-  
-  def length_of_array
-    @programming_languages.length
-      
-  end
+def length_of_array
+ @programming_languages.length
+end
